7ce9306c5ee6ddc36ca3e63db306461ceccddd8e
Cyrille Bomba <c.bombangong@fulll.fr>
2021-11-29 15:48:15 +0100

    useState for to get values

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState } from "react";
 
-const Search = () => {
-  return <div>input to come</div>;
+const Search: React.FC = () => {
+  const [searchItem, setSearchItem] = useState("");
+
+  const handleChange = (e: any) => {
+    setSearchItem(e.target.value);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="search..."
+        value={searchItem}
+        onChange={handleChange}
+      />
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </>
+  );
 };
 
 export default Search;
