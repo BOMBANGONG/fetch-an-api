ea18b6cb068d124644f07b6ff415c8029c9fb2a7
Cyrille Bomba <c.bombangong@fulll.fr>
2021-12-07 10:31:50 +0100

    fetching done

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -19,10 +19,23 @@ const initialState: Response = {
 const Search: React.FC = () => {
   const [users, setUsers] = useState("");
   const [repos, setRepos] = useState(initialState);
+  const [rateLimit, setRateLimit] = useState(Number.MAX_SAFE_INTEGER);
+
+  const isOffLimit = rateLimit < 1;
 
   const handleChange = (e: any) => {
     setUsers(e.target.value);
   };
+
+  useEffect(() => {
+    let timeoutId: unknown;
+    if (isOffLimit) {
+      timeoutId = setTimeout(() => {
+        setRateLimit(Number.MAX_SAFE_INTEGER);
+      }, 5000);
+    }
+    return () => clearTimeout(timeoutId as NodeJS.Timeout);
+  }, [isOffLimit]);
 
   useEffect(() => {
     if (!users) {
@@ -30,6 +43,13 @@ const Search: React.FC = () => {
     }
     fetch("https://api.github.com/search/users?q=" + users)
       .then((response) => {
+        let apiCallsRemaining = parseInt(
+          response.headers.get("x-ratelimit-remaining") || ""
+        );
+
+        if (Number.isNaN(apiCallsRemaining))
+          apiCallsRemaining = Number.MAX_SAFE_INTEGER;
+        setRateLimit(apiCallsRemaining);
         return response.json();
       })
       .then((data) => {
@@ -50,6 +70,7 @@ const Search: React.FC = () => {
         type="text"
         placeholder=" search github user"
         onChange={debouncedOnChange}
+        disabled={isOffLimit}
       />
       {repos.message ? (
         repos.message
